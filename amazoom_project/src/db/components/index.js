ee4bbe23362205b2121c3665af99df78776b634a
Silver <jice391@gmail.com>
2023-02-24 12:49:15 -0500

    Changed file

---
+++ amazoom_project/src/db/components/index.js
@@ -0,0 +1,8 @@
+export { default as App } from "./App";
+export { default as Products } from "./Products";
+export { default as loginLogout } from "./loginLogout";
+export { default as ModifyPost } from "./ModifyPost";
+export { default as supplier } from "./supplier";
+export { default as user } from "./user";
+export { default as shopping_cart } from "./shopping_cart";
+
