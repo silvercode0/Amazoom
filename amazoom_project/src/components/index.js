0dca49db5703c690c8870983a00d775f873d4cc6
Silver <jice391@gmail.com>
2023-02-27 20:23:19 -0500

    updates

--- amazoom_project/src/components/index.js
+++ amazoom_project/src/components/index.js
@@ -1,10 +1,11 @@
 export { default as App } from "./App";
 export { default as Products } from "./Products";
-export { default as login} from "./login";
-export { default as logout} from "./logout";
-export { default as ModifyPost } from "./ModifyPost";
+export { default as login } from "./login";
+export { default as logout } from "./logout";
+export { default as ModifyReview } from "./ModifyReview";
 export { default as supplier } from "./supplier";
 export { default as user } from "./user";
 export { default as shopping_cart } from "./shopping_cart";
 export { default as home } from "./home";
+export { default as register } from "./register";
 
