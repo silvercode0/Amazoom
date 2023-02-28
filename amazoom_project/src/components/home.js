c05864375dfd1799255f702aaf05a9667408cdee
Silver <jice391@gmail.com>
2023-02-27 20:42:06 -0500

    updates

--- amazoom_project/src/components/home.js
+++ amazoom_project/src/components/home.js
@@ -1,16 +1,1 @@
-// import React from "react";
-// import "./Home.css";
-
-// const Home = () => {
-//   return (
-//     <div id="wholeContainer">
-//       <video src="/videos/video1.mp4" autoPlay loop muted />
-//       <h1 className="header">DimTECH</h1>
-//       <h2 className="subheader">
-//         Products from tomorrow, <u>today!</u>
-//       </h2>
-//     </div>
-//   );
-// };
-
-// export default Home;
+// Home // -- Regular Route -- Base
