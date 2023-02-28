0b4f6b760668b419ab5c40ba358d2950a8c534ec
Silver <jice391@gmail.com>
2023-02-27 21:29:38 -0500

    added an api route

--- amazoom_project/src/api/index.js
+++ amazoom_project/src/api/index.js
@@ -1,1 +1,16 @@
 const BASE_URL = "http://localhost:3000/api/";
+
+// Products //
+async function getAllProductsById(productId) {
+    try {
+        const fetchURL = await fetch(`${BASE_URL}products/${productId}`);
+        const resultURL = await fetchURL.json();
+
+        console.log("done fetching url for products");
+        return resultURL;
+    } catch (error) {
+        console.log("error fetching url for products");
+        throw error; 
+
+    }
+}
