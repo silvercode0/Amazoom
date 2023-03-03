24c110e886c122c11ecda7a226c3d15ccb97770a
Silver <jice391@gmail.com>
2023-03-02 18:11:52 -0800

    updts

--- amazoom_project/src/db/home.js
+++ amazoom_project/src/db/home.js
@@ -1,1 +1,17 @@
 // Home // -- Regular Route -- Base
+
+import Navbar from "./navbar";
+export function Home({ setLoggedIn, loggedIn }) {
+  const user = localStorage.getItem("user");
+  return (
+    <>
+      <Navbar setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
+      <div className={styles.title}>
+        <h1>Amazoom {user} </h1>
+        <h3>
+          Go checkout some of our nice Cars !
+        </h3>
+      </div>
+    </>
+  );
+}
