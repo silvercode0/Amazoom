48ce3a0b6ccabd5846aa5abed93252a7cfbe3832
Silver <jice391@gmail.com>
2023-03-09 17:34:31 -0800

    db changes

--- amazoom_project/src/seedData.js
+++ amazoom_project/src/seedData.js
@@ -1,70 +1,101 @@
 // Const Variables go here that will be used in this file
 
-const {
-  createUser,
-  createProduct,
-  createShoppingCart,
-  getShoppingCart,
-  getAllProducts,
-  addSupplierToProduct
-} = require('./');
+// const {
+//   createUser,
+//   createProduct,
+//   createShoppingCart,
+//   getShoppingCart,
+//   getAllProducts,
+//   addSupplierToProduct
+// } = require('./');
 
-const client = require("./client")
-
+const { createUser } = require( "./users");
+const{createProduct}=require("./products")
+const{createReviews}=require('./reviews')
+const client = require("./client");
+const { createShoppingCart } = require("./shopping_cart");
+const{addItemsToCart}=require("./shopping_cartitems")
 async function dropTables() {
-    try {
-      console.log("Dropping All Tables...")
-  
-      await client.query(`
-      DROP TABLE IF EXISTS supplier;
-      DROP TABLE IF EXISTS shopping_cart;
-      DROP TABLE IF EXISTS product;
+  try {
+    console.log('Dropping All Tables...');
+    await  client.query(`
+      DROP TABLE IF EXISTS cart_items;
+      DROP TABLE IF EXISTS cart;
+      DROP TABLE IF EXISTS ratings;
+      DROP TABLE IF EXISTS cars;
       DROP TABLE IF EXISTS users;
       `)
-  
-      console.log("Done Droping Tables")
-    } catch (error) {
-      throw error;
-    }
-    // drop all tables, in the correct order '
-}
+    console.log('finished dropping tables')
+  } catch(error) {
+    console.log("error dropping tables")
+  }
+};
 
 async function createTables() {
-
+  console.log('Starting to build tables')
   try {
-   console.log("Starting to build tables...")
-  // create all tables, in the correct order
-    await client.query(`
+    await  client.query(`
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL
-      );
-      CREATE TABLE product (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL,
-        description TEXT NOT NULL
-      );
-      CREATE TABLE shopping_cart (
-        id SERIAL PRIMARY KEY,
-        "shopping_cartId" INTEGER REFERENCES users(id),
-        "isPublic" BOOLEAN DEFAULT false,
-        name VARCHAR(255) UNIQUE NOT NULL,
-        goal TEXT NOT NULL
-      );
-      CREATE TABLE supplier (
-        id SERIAL PRIMARY KEY,
-        "supplierId" INTEGER REFERENCES supplier (id),
-        "productId" INTEGER REFERENCES product (id),
-        duration INTEGER, 
-        count INTEGER,
-        UNIQUE ("supplierId","productId")
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL,
+        email varchar(255) UNIQUE NOT NULL,
+        firstname varchar(255) NOT NULL,
+        lastname varchar(255) NOT NULL,
+        street varchar(255) NOT NULL,
+        city varchar(255) NOT NULL,
+        state varchar(255) NOT NULL,
+        zip varchar(255) UNIQUE NOT NULL,
+        phone varchar(255) UNIQUE NOT NULL,
+        isadmin BOOLEAN DEFAULT FALSE
       );
     `)
-    console.log("Done Creating Tables")
+    await client.query(`
+      CREATE TABLE cars (
+        id SERIAL PRIMARY KEY,
+        make varchar(255) NOT NULL,
+        model varchar(255) NOT NULL,
+        year integer,
+        price DECIMAL(10, 2) NOT NULL,
+        inventory integer,
+        condition varchar(255) NOT NULL,
+        engine varchar(255) NOT NULL,
+        transmission varchar(255) NOT NULL,
+        drivetrain varchar(255) NOT NULL,
+        fuel varchar(255) NOT NULL,
+        exteriorcolor varchar(255) NOT NULL,
+        interiorcolor varchar(255) NOT NULL,
+        description varchar(255) NOT NULL
+      );
+    `)
+    await client.query(`
+      CREATE TABLE ratings (
+        id SERIAL PRIMARY KEY,
+        userid integer REFERENCES users(id),
+        vehicleid integer REFERENCES cars(id),
+        review varchar(255) NOT NULL,
+        stars integer
+      );
+    `),
+    await client.query(`
+      CREATE TABLE cart (
+        id SERIAL PRIMARY KEY,
+        user_id integer REFERENCES users(id),
+        total_price varchar(255),
+        date_purchased varchar(255),
+        transactioncomplete BOOLEAN DEFAULT FALSE
+      );
+    `),
+    await client.query(`
+      CREATE TABLE cart_items (
+        id SERIAL PRIMARY KEY,
+        cart_id integer REFERENCES cart(id),
+        vehicle_id integer REFERENCES cars(id),
+        quantity integer DEFAULT 0
+      );
+    `),
+    console.log('finished creating tables')
   } catch (error) {
-    throw error;
+    console.log(error)
   }
-}
-
-
+};
