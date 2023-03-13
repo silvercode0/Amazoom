a96a47621cfd4b694e3bd654a80b6f86898f4e1d
Silver <jice391@gmail.com>
2023-03-13 11:49:51 -0700

    changes

--- amazoom_project/src/supplier.js
+++ amazoom_project/src/supplier.js
@@ -1,11 +1,10 @@
-
+// LET PEOPLE KNOW WHO WE ARE // 
 const Supplier = () => {
     return ( 
         <div className="supplier-container">
           <h1> Supplier </h1>
           <Link id="Supplier" to="/Supplier">
             About Us
-            
           </Link>
         </div>
     )
