49ed9cea07e0b915bb2b0b8dd4183a0f7e79049d
Silver <jice391@gmail.com>
2023-03-13 11:44:44 -0700

    updts

---
+++ amazoom_project/src/supplier.js
@@ -0,0 +1,12 @@
+
+const Supplier = () => {
+    return ( 
+        <div className="supplier-container">
+          <h1> Supplier </h1>
+          <Link id="Supplier" to="/Supplier">
+            About Us
+            
+          </Link>
+        </div>
+    )
+}
