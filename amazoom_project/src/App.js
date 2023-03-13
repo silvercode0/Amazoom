162168a28525ed492f604ef21f10bb8d2b625db4
RaveBlue <my-raveblue21@yahoo.com>
2023-03-13 17:29:33 -0600

    App

--- amazoom_project/src/App.js
+++ amazoom_project/src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
-<<<<<<< HEAD
 import './Navbar.css';
 import './App.css';
 import Home from './Home';
@@ -23,33 +22,6 @@ function App() {
     <Route path="products" element={<Products />} />
   </Routes>
   </div>
-=======
-import "./App.css"
-import Login from "./Login"
-// import Supplier from "./Supplier"
-// import BuyOptions from "./BuyOptions"
-
-function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <Login></Login>
-        <p class="test2">
-          TESTING
-        </p>
-      </header>
-      <footer>
-        {/* <BuyOptions></BuyOptions>
-        <Supplier></Supplier> */}
-      </footer>
-    </div>
->>>>>>> 1d54af54c3f92775aa6ea8dc703c46ab48ca8cba
   );
 }
 
